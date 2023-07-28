10e52d60b53cfd219a49ea03d10cdcede8921dcd
Mohamed <s2000588@edu.bc.fi>
2023-07-28 20:09:21 +0300

    update header components & add scroll to top component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,12 @@ import "./App.css";
 import { Outlet } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import { Header } from "./components/Header/Index";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <Toaster />
       <main className='py-3'>
--- frontend/src/components/Header/Index.tsx
+++ frontend/src/components/Header/Index.tsx
@@ -1,13 +1,17 @@
 import { useEffect } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
-import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+import { AiOutlineLogout } from "react-icons/ai";
+import { BsFillPersonPlusFill } from "react-icons/bs";
+import { BiLogIn } from "react-icons/bi";
+import { AiOutlinePlus } from "react-icons/ai";
+import { BsGrid } from "react-icons/bs";
+import { HiOutlineUserGroup } from "react-icons/hi";
 import logo from "../../assets/logo.png";
 import { Nav, Navbar, Container, Badge, NavDropdown } from "react-bootstrap";
 import { useAppDispatch, useAppSelector } from "../../redux/app/store";
-import { AuthUser } from "../../interfaces/AuthInterface";
-import { logout, userProfile } from "../../redux/feature/Auth/authSlice";
+import { logout } from "../../redux/feature/Auth/authSlice";
 import { getSavedRecipes } from "../../redux/feature/Recipe/recipeSlice";
 
 export const Header = () => {
@@ -30,7 +34,7 @@ export const Header = () => {
   };
 
   return (
-    <header>
+    <header style={{ zIndex: 1, position: "sticky", top: 0 }}>
       <Navbar bg='success' variant='' expand='md' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
@@ -41,7 +45,10 @@ export const Header = () => {
 
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ms-auto' style={{ fontSize: "1.2rem" }}>
+            <Nav
+              className='ms-auto'
+              style={{ fontSize: "1.1rem", fontWeight: "bold" }}
+            >
               {/* <SearchBox /> */}
               <LinkContainer to='/'>
                 <Nav.Link>Home</Nav.Link>
@@ -49,49 +56,78 @@ export const Header = () => {
               <LinkContainer to='/saved-recipes'>
                 <Nav.Link>Categories</Nav.Link>
               </LinkContainer>
-              <LinkContainer to='/saved-recipes'>
-                <Nav.Link>
-                  Saved Recipes
-                  {savedRecipes?.length > 0 && (
-                    <Badge pill bg='dark' className='mx-1'>
-                      {savedRecipes?.length}
-                    </Badge>
-                  )}
-                </Nav.Link>
-              </LinkContainer>
-
+              {user && (
+                <LinkContainer to='/saved-recipes'>
+                  <Nav.Link className='d-flex align-items-center justify-content-center'>
+                    Saved Recipes
+                    {savedRecipes?.length > 0 && (
+                      <Badge pill bg='primary' className='mx-1'>
+                        {savedRecipes?.length}
+                      </Badge>
+                    )}
+                  </Nav.Link>
+                </LinkContainer>
+              )}
               {user ? (
                 <NavDropdown title={user.name} id='username'>
                   <LinkContainer to='/profile'>
-                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                    <NavDropdown.Item>
+                      <FaUser className='me-2' />
+                      Profile
+                    </NavDropdown.Item>
                   </LinkContainer>
                   <LinkContainer to='/add-recipe'>
-                    <NavDropdown.Item>Add recipe</NavDropdown.Item>
+                    <NavDropdown.Item>
+                      <AiOutlinePlus className='me-2' />
+                      Add recipe
+                    </NavDropdown.Item>
                   </LinkContainer>
 
                   {user && user.role === "admin" && (
                     <>
                       <LinkContainer to='/admin/add-category'>
-                        <NavDropdown.Item>Add category</NavDropdown.Item>
+                        <NavDropdown.Item>
+                          <AiOutlinePlus className='me-2' />
+                          Add category
+                        </NavDropdown.Item>
                       </LinkContainer>
                       <LinkContainer to='/admin/all-categories'>
-                        <NavDropdown.Item>All categories</NavDropdown.Item>
+                        <NavDropdown.Item>
+                          <BsGrid className='me-2' />
+                          Admin categories
+                        </NavDropdown.Item>
                       </LinkContainer>
                       <LinkContainer to='/admin/userlist'>
-                        <NavDropdown.Item>Users</NavDropdown.Item>
+                        <NavDropdown.Item>
+                          <HiOutlineUserGroup className='me-2' />
+                          Users
+                        </NavDropdown.Item>
                       </LinkContainer>
                     </>
                   )}
-                  <NavDropdown.Item onClick={handleLogout}>
+                  <NavDropdown.Item
+                    onClick={handleLogout}
+                    className='text-danger'
+                  >
+                    <AiOutlineLogout className='me-2' />
                     Logout
                   </NavDropdown.Item>
                 </NavDropdown>
               ) : (
-                <LinkContainer to='/login'>
-                  <Nav.Link>
-                    <FaUser /> Sign In
-                  </Nav.Link>
-                </LinkContainer>
+                <>
+                  <LinkContainer to='/login'>
+                    <Nav.Link className='d-flex align-items-center justify-content-center'>
+                      <BiLogIn className='me-1' />
+                      Login
+                    </Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to='/register'>
+                    <Nav.Link className='d-flex align-items-center justify-content-center'>
+                      <BsFillPersonPlusFill className='me-1' />
+                      Register
+                    </Nav.Link>
+                  </LinkContainer>
+                </>
               )}
             </Nav>
           </Navbar.Collapse>
